c6b0ed35277da794cef8df7e7c1e48d48156c71d
ram10g <71540871+ram10g@users.noreply.github.com>
2022-07-26 13:59:23 +0530

    Add files via upload

---
+++ Project3/Project3.java
@@ -0,0 +1,13 @@
+package Project3;
+
+public class Project3 {
+	static void myMethod() {
+        System.out.println("verified implementation of methods and name of this methods is: myMethod!");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		myMethod();
+
+	}
+
+}
