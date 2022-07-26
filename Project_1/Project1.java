f8ad56485519a7bb47d5f54ed460ee29ae5dcde7
ram10g <71540871+ram10g@users.noreply.github.com>
2022-07-26 13:51:45 +0530

    Project_1

---
+++ Project_1/Project1.java
@@ -0,0 +1,18 @@
+package Project1;
+
+public class Project1 {
+	public static void main(String[] args) {
+		//implicit casting
+		int num1=10;
+		System.out.println("The integer value: " + num1);
+		double data1 = num1;
+	    System.out.println("The double value: " + data1);
+	    //explicit casting
+	    double num2 = 10.99;
+	    System.out.println("The double value: " + num2);
+	    int data2 = (int)num2;
+	    System.out.println("The integer value: " + data2);
+	}
+	
+
+}
