bd6af4bcc612d1b62b1ff807e018cc959287c5a0
ram10g <71540871+ram10g@users.noreply.github.com>
2022-07-26 13:58:10 +0530

    Add files via upload

---
+++ Project2/Project2.java
@@ -0,0 +1,13 @@
+package Project2;
+
+import Project1.Project_2;
+
+public class Project2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Project_2 obj =new Project_2();
+		obj.msg();
+	}
+
+}
